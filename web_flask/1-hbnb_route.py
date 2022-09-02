3df27f13ee1551b0a7bc60aa0e14b6720c2d020e
Agustin Labadie <labalabadie@gmail.com>
2022-09-02 22:15:25 +0000

    A

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Flask,baby"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+	return "<p>Hello HBNB!</p>"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+	return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5000)
+
