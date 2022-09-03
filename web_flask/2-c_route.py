3c75399a49320e70946a28351b33d173497305bd
Agustin Labadie <labalabadie@gmail.com>
2022-09-03 15:42:58 +0000

    all pep8 so far

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,18 +6,20 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-	return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
-	return "<p>Hello HBNB!</p>"
+    return "HBNB"
+
 
 @app.route("/c/<path:text>", strict_slashes=False)
 def C(text):
-	return "C " + text
+    return "C " + text
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
